e3f8dd9eb71e1d0f943db14145ecb8e84d120b78
An Pham <anpham6@gmail.com>
2020-04-24 10:41:02 -0700

    Absolute elements did not adjust for trailing or leading linebreaks.

--- android-framework/src/extensions/delegate/positive-x.ts
+++ android-framework/src/extensions/delegate/positive-x.ts
@@ -18,118 +18,125 @@ const checkMarginRight = (node: View, item: View) => item.marginRight < 0 && (no
 const checkMarginTop = (node: View, item: View) => item.marginTop < 0 && (node.originalRoot || item.linear.top < Math.floor(node.box.top));
 const checkMarginBottom = (node: View, item: View) => item.marginBottom < 0 && (node.originalRoot || item.linear.bottom > Math.ceil(node.box.bottom));
 
-function setFixedNodes(node: View, contentBox: boolean) {
-    const documentBody = node.documentBody;
-    if (!contentBox && !documentBody) {
-        return false;
-    }
-    const documentRoot = node.originalRoot;
-    const expandBody = documentBody && node.positionStatic;
-    const children = new Set<View>();
-    const paddingTop = node.paddingTop + (documentBody ? node.marginTop : 0);
-    const paddingRight = node.paddingRight + (documentBody ? node.marginRight : 0);
-    const paddingBottom = node.paddingBottom + (documentBody ? node.marginBottom : 0);
-    const paddingLeft = node.paddingLeft + (documentBody ? node.marginLeft : 0);
-    let right = false, bottom = false;
-    node.each((item: View) => {
-        const fixed = documentRoot && item.css('position') === 'fixed';
-        if (item.pageFlow || !contentBox && !fixed) {
-            return;
-        }
-        const fixedPosition = fixed && item.autoPosition;
-        if (item.hasPX('left') || fixedPosition) {
-            if (documentBody && (item.css('width') === '100%' || item.css('minWidth') === '100%')) {
-                children.add(item);
-                right = true;
-            }
-            else {
-                const value = item.left;
-                if ((value >= 0 || documentRoot) && value < paddingLeft) {
-                    children.add(item);
-                }
-                else if (value < 0 && node.marginLeft > 0) {
-                    children.add(item);
-                }
-                else if (!item.hasPX('right') && checkMarginLeft(node, item)) {
-                    children.add(item);
-                }
-            }
-        }
-        else if (item.hasPX('right')) {
-            if (expandBody) {
-                children.add(item);
-                right = true;
-            }
-            else {
-                const value = item.right;
-                if ((value >= 0 || documentRoot) && value < paddingRight) {
-                    children.add(item);
-                }
-                else if (value < 0 && node.marginRight > 0) {
-                    children.add(item);
-                }
-                else if (checkMarginRight(node, item)) {
-                    children.add(item);
-                }
-            }
-        }
-        else if (checkMarginLeft(node, item)) {
-            children.add(item);
-        }
-        if (item.hasPX('top') || fixedPosition) {
-            if (documentBody && (item.css('height') === '100%' || item.css('minHeight') === '100%')) {
-                children.add(item);
-                bottom = true;
-            }
-            else {
-                const value = item.top;
-                if ((value >= 0 || documentRoot) && value < paddingTop) {
-                    children.add(item);
-                }
-                else if (value < 0 && node.marginTop > 0) {
-                    children.add(item);
-                }
-                else if (!item.hasPX('bottom') && checkMarginTop(node, item)) {
-                    children.add(item);
-                }
-            }
-        }
-        else if (item.hasPX('bottom')) {
-            if (expandBody) {
-                children.add(item);
-                bottom = true;
-            }
-            else {
-                const value = item.bottom;
-                if ((value >= 0 || documentRoot) && value < paddingBottom) {
-                    children.add(item);
-                }
-                else if (value < 0 && node.marginBottom > 0) {
-                    children.add(item);
-                }
-                else if (checkMarginBottom(node, item)) {
-                    children.add(item);
-                }
-            }
-        }
-        else if (checkMarginTop(node, item)) {
-            children.add(item);
-        }
-    });
-    if (children.size) {
-        node.data(EXT_ANDROID.DELEGATE_POSITIVEX, 'mainData', { children: Array.from(children), right, bottom });
-        return true;
-    }
-    return false;
-}
-
 export default class PositiveX<T extends View> extends squared.base.ExtensionUI<T> {
     public is(node: T) {
         return node.length > 0;
     }
 
     public condition(node: T) {
-        return setFixedNodes(node, node.contentBoxWidth > 0 || node.contentBoxHeight > 0 || node.marginTop > 0 || node.marginRight > 0 || node.marginBottom > 0 || node.marginLeft > 0);
+        const { documentBody, lastStaticChild } = node;
+        let contentBox = node.contentBoxWidth > 0 || node.contentBoxHeight > 0 || node.marginTop !== 0 || node.marginRight !== 0 || node.marginBottom !== 0 || node.marginLeft !== 0;
+        let aboveInvalid = false;
+        let belowInvalid = false;
+        if (node.firstStaticChild?.lineBreak) {
+            aboveInvalid = true;
+            contentBox = true;
+        }
+        if (lastStaticChild?.lineBreak && lastStaticChild.previousSibling?.blockStatic) {
+            contentBox = true;
+            belowInvalid = true;
+        }
+        if (!documentBody && !contentBox) {
+            return false;
+        }
+        const originalRoot = node.originalRoot;
+        const expandBody = documentBody && node.positionStatic;
+        const children = new Set<View>();
+        const paddingTop = node.paddingTop + (documentBody ? node.marginTop : 0);
+        const paddingRight = node.paddingRight + (documentBody ? node.marginRight : 0);
+        const paddingBottom = node.paddingBottom + (documentBody ? node.marginBottom : 0);
+        const paddingLeft = node.paddingLeft + (documentBody ? node.marginLeft : 0);
+        let right = false, bottom = false;
+        node.each((item: View) => {
+            const fixed = originalRoot && item.css('position') === 'fixed';
+            if (item.pageFlow || !contentBox && !fixed) {
+                return;
+            }
+            const fixedPosition = fixed && item.autoPosition;
+            if (item.hasPX('left') || fixedPosition) {
+                if (documentBody && (item.css('width') === '100%' || item.css('minWidth') === '100%')) {
+                    children.add(item);
+                    right = true;
+                }
+                else {
+                    const value = item.left;
+                    if ((value >= 0 || originalRoot) && value < paddingLeft) {
+                        children.add(item);
+                    }
+                    else if (value < 0 && node.marginLeft > 0) {
+                        children.add(item);
+                    }
+                    else if (!item.hasPX('right') && checkMarginLeft(node, item)) {
+                        children.add(item);
+                    }
+                }
+            }
+            else if (item.hasPX('right')) {
+                if (expandBody) {
+                    children.add(item);
+                    right = true;
+                }
+                else {
+                    const value = item.right;
+                    if ((value >= 0 || originalRoot) && value < paddingRight) {
+                        children.add(item);
+                    }
+                    else if (value < 0 && node.marginRight > 0) {
+                        children.add(item);
+                    }
+                    else if (checkMarginRight(node, item)) {
+                        children.add(item);
+                    }
+                }
+            }
+            else if (checkMarginLeft(node, item)) {
+                children.add(item);
+            }
+            if (item.hasPX('top') || fixedPosition) {
+                if (documentBody && (item.css('height') === '100%' || item.css('minHeight') === '100%')) {
+                    children.add(item);
+                    bottom = true;
+                }
+                else {
+                    const value = item.top;
+                    if ((value >= 0 || originalRoot) && (value < paddingTop || aboveInvalid)) {
+                        children.add(item);
+                    }
+                    else if (value < 0 && node.marginTop > 0) {
+                        children.add(item);
+                    }
+                    else if (!item.hasPX('bottom') && checkMarginTop(node, item)) {
+                        children.add(item);
+                    }
+                }
+            }
+            else if (item.hasPX('bottom')) {
+                if (expandBody) {
+                    children.add(item);
+                    bottom = true;
+                }
+                else {
+                    const value = item.bottom;
+                    if ((value >= 0 || originalRoot) && (value < paddingBottom || belowInvalid)) {
+                        children.add(item);
+                    }
+                    else if (value < 0 && node.marginBottom > 0) {
+                        children.add(item);
+                    }
+                    else if (checkMarginBottom(node, item)) {
+                        children.add(item);
+                    }
+                }
+            }
+            else if (checkMarginTop(node, item)) {
+                children.add(item);
+            }
+        });
+        if (children.size) {
+            node.data(EXT_ANDROID.DELEGATE_POSITIVEX, 'mainData', { children: Array.from(children), right, bottom });
+            return true;
+        }
+        return false;
     }
 
     public processNode(node: T, parent: T) {
