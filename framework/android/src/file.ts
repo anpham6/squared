5c87801b9863a8956ba1f8588901da2abda609cc
An Pham <anpham6@gmail.com>
2021-04-15 10:39:59 -0700

    Merge: Commit 9b03140 (master)

--- framework/android/src/file.ts
+++ framework/android/src/file.ts
@@ -52,13 +52,9 @@ function getImageAssets(this: Resource<View>, resourceId: number, pathname: stri
         const uri = items[i];
         const filename = items[i + 2];
         let mimeType: Undef<string>,
-            commands: Undef<string[]>,
-            compress: Undef<CompressFormat[]>;
+            commands: Undef<string[]>;
         if (endsWith(filename, '.unknown')) {
             mimeType = 'image/unknown';
-            if (compressing) {
-                compress = [{ format: 'png' }];
-            }
         }
         else if (convertImages) {
             switch (mimeType = parseMimeType(filename)) {
@@ -70,19 +66,8 @@ function getImageAssets(this: Resource<View>, resourceId: number, pathname: stri
                 case 'image/tiff':
                 case 'image/unknown':
                     for (const value of convertImages.trim().toLowerCase().split(/\s*::\s*/)) {
-                        const match = /^[a-z]+/.exec(value);
-                        if (match) {
-                            switch (match[0]) {
-                                case 'png':
-                                case 'jpeg':
-                                case 'webp':
-                                case 'bmp':
-                                    (commands ||= []).push(value);
-                                    if (compressing && !compress && Resource.canCompressImage(filename, match[0])) {
-                                        compress = [{ format: 'png' }];
-                                    }
-                                    break;
-                            }
+                        if (/^(?:png|jpeg|webp|bmp)/.test(value)) {
+                            (commands ||= []).push(value);
                         }
                     }
                     break;
@@ -94,7 +79,7 @@ function getImageAssets(this: Resource<View>, resourceId: number, pathname: stri
             filename,
             mimeType,
             commands,
-            compress,
+            compress: compressing ? [{ format: 'png' }] : undefined,
             uri,
             document: File.copyDocument(document),
             tasks: image && image.tasks
